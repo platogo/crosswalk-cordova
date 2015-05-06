fdfce1b3f35a7e0e5535c020356a85e1dfa81d64
Florian Landerl <florian@platogo.com>
2015-05-06 17:27:51 +0200

    Always use fullscreen even if immersive mode is supported

--- arm/framework/src/org/apache/cordova/CordovaActivity.java
+++ arm/framework/src/org/apache/cordova/CordovaActivity.java
@@ -299,20 +299,8 @@ public class CordovaActivity extends Activity implements CordovaInterface {
      */
     @SuppressLint("NewApi")
     private void toggleFullscreen(Window window) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            final int uiOptions =
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-
-            window.getDecorView().setSystemUiVisibility(uiOptions);
-        } else {
-            window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        }
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
     /**
